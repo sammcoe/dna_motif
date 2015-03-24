823fa223f72ffa80bce5a4c477f99bee59df9806
Sam Coe <samuel.m.coe@gmail.com>
2015-03-23 19:31:39 -0500

    Mapper function now pulls apart file into unique sequences

--- mapper.py
+++ mapper.py
@@ -7,18 +7,27 @@
 
 import sys
 
-def get_sequences(file, l):
+def get_sequences(file, length):
   # Get the total length of the file
   # This will be used to determine when all sequences have been read
-  chars = (len(file) - l)
+  data = file.read()
   start = 0
-  end = 7
   # Increment through entire file, through every
   # possible sequence.
   # If the first line is 'tacatgact', then first pass is
   # 'tacatgac' and the second pass is 'acatgact'
-  for chars in file:
-    yield file[start:end]
+  for char in data:
+    yield data[start:length]
     start += 1
-    end += 1
+    length += 1
 
+def main():
+  # Get sequences from file
+  data_set = open('promoters_data_clean.txt')
+  sequences = get_sequences(data_set, 8)
+
+  for sequence in sequences:
+    print '%s\t%d' % (sequence, 1)
+
+if __name__ == "__main__":
+  main()
